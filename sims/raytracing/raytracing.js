993b57a505d5708960262cfc251c46e6253eb3f9
ntrupin <natrupin@gmail.com>
2023-05-24 17:49:37 -0400

    updates

--- sims/raytracing/raytracing.js
+++ sims/raytracing/raytracing.js
@@ -140,7 +140,7 @@ class Entity {
         world.p.line(ox, oy, strLxr.x, strLxr.y);
 
         if (i < 0) { 
-            const perpVec = world.p.createVector(5000, 5000*((H + h)/i));
+            const perpVec = world.p.createVector(5000, 5000*((H + h)/(i - W)));
             const perpEnd = p5.Vector.lerp(perpMid, perpVec, this.p < 1 ? 0 : this.p - 1);
             const lfVec = world.p.createVector(5000, iy);
             const lfEnd = p5.Vector.lerp(lfMid, lfVec, this.p < 1 ? 0 : this.p - 1);
